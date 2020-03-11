603c91b40b17a2db76b99b9693d4704be0f006ad
Zuguang Gu <jokergoo@gmail.com>
2020-03-11 13:57:37 +0100

    Update monitor.R

--- R/monitor.R
+++ R/monitor.R
@@ -186,9 +186,9 @@ convert_to_POSIXlt = function(x) {
 
     if(is.null(bsub_opt$parse_time)) {
 
-        if(all(grepl("^\\w+\\s+\\d+\\s+\\d+:\\d+$", x[1]))) { # Dec 1 18:00
+        if(any(grepl("^\\w+\\s+\\d+\\s+\\d+:\\d+$", x))) { # Dec 1 18:00
             t = as.POSIXlt(x, format = "%b %d %H:%M")
-        } else if(all(grepl("^\\w+\\s+\\d+\\s+\\d+:\\d+:\\d+$", x[1]))) { # Dec 1 18:00:00
+        } else if(any(grepl("^\\w+\\s+\\d+\\s+\\d+:\\d+:\\d+$", x))) { # Dec 1 18:00:00
             t = as.POSIXlt(x, format = "%b %d %H:%M:%S")
         } else {                                        # Dec 1 18:00:00 2019
             t = as.POSIXlt(x, format = "%b %d %H:%M:%S %Y")
